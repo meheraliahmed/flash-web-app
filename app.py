8daa84dd1348bc0630c1f4090d00b351b0fb45fb
meheraliahmed <67927192+meheraliahmed@users.noreply.github.com>
2025-02-07 16:03:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Azure Web App!"
+    return "Hello from Azure Web App!!!!!!!!"
 
 if __name__ == "__main__":
     app.run(debug=True)
